8f39b5cdec855bd25c1204e518d8b3b98497cbed
Weslley Moura <schno@WeslleyMoura.localdomain>
2022-10-24 17:33:49 -0300

    Iniciando exercicio dia 4 - javascript

---
+++ Nova pasta/script.js
@@ -0,0 +1,33 @@
+const myName = 'WeslleyMoura';
+const birthCity = 'Paulista';
+let birthYear = '1998';
+console.log(myName);
+console.log(birthCity);
+
+birthYear = 2040;
+console.log(birthYear);
+
+// segunda parte dos exercicios
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+console.log(typeof patientId);
+console.log(typeof patientEmail);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientAge);
+console.log(typeof '-----------');
+
+const base = 5;
+const heigth = 8;
+const area = base * heigth;
+const perimetro = 5 + 5 + 8 + 8;
+console.log(base);
+console.log(heigth);
+console.log(area);
+console.log(perimetro);
